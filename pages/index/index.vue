606086c508d0daa4d08f05dc987e92d81a0d1a55
guo <2246338405@qq.com>
2023-03-07 22:02:27 +0800

    第六次提交
    使用uni.request获取网络请求渲染到页面中

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,11 +1,14 @@
 <template>
 	<view class="home">
 		<scroll-view scroll-x class="navscroll">
-			<view class="item" :class="index == navIndex ? 'active' : ' '" v-for="(item,index) in 10" @click="clickNav(index)">国内</view>
+			<view class="item" :class="index == navIndex ? 'active' : ' '" v-for="(item,index) in navArr"
+				@click="clickNav(index)" :key="item.id">
+				{{item.classname}}
+			</view>
 		</scroll-view>
 		<view class="content">
-			<view class="row" v-for="item in 10">
-				<newsbox @click.native="goDetail"></newsbox>
+			<view class="row" v-for="item in newsArr" :key="item.id">
+				<newsbox :item="item" @click.native="goDetail"></newsbox>
 			</view>
 		</view>
 	</view>
@@ -15,24 +18,53 @@
 	export default {
 		data() {
 			return {
-				navIndex:0
+				navIndex: 0,
+				navArr: [],
+				newsArr:[]
 			}
 		},
 		onLoad() {
-
+			this.getNavDat();
+			this.getNewsData();
 		},
 		methods: {
 			// 点击导航切换
-			clickNav(index){
+			clickNav(index) {
 				this.navIndex = index
 			},
-			
+
 			// 跳转到详情页
-			goDetail(){
+			goDetail() {
 				uni.navigateTo({
-					url:"/pages/detail/detail"
+					url: "/pages/detail/detail"
 				})
-			}
+			},
+
+			// 获取导航列表数据
+			getNavDat() {
+				uni.request({
+					url: "https://ku.qingnian8.com/dataApi/news/navlist.php",
+					success: res => {
+						console.log(res);
+						this.navArr = res.data
+					}
+				})
+			},
+
+			// 获取新闻列表数据
+			getNewsData() {
+				uni.request({
+					url: "https://ku.qingnian8.com/dataApi/news/newslist.php",
+					data:{
+						num:5,
+						cid:50,
+					}
+					success: res => {
+						console.log(res);
+						this.newsArr = res.data
+					}
+				})
+			},
 		}
 	}
 </script>
@@ -43,7 +75,7 @@
 		background-color: #f7f8fa;
 		white-space: nowrap;
 		position: fixed;
-		top:var(--window-top);
+		top: var(--window-top);
 		z-index: 10;
 
 		/deep/::-webkit-scrollbar {
@@ -61,7 +93,8 @@
 			line-height: 100rpx;
 			padding: 0 30rpx;
 			color: #333;
-			&.active{
+
+			&.active {
 				color: #31C27C;
 			}
 		}
@@ -70,6 +103,7 @@
 	.content {
 		padding: 30rpx;
 		padding-top: 130rpx;
+
 		.row {
 			border-bottom: 1px dotted #efefef;
 			padding: 20rpx 0;
