d4bb1ee76733f2d74efb60c284d20011f29b0280
guo <2246338405@qq.com>
2023-03-10 01:28:01 +0800

    第十一次提交
    优化细节，增加 loading 加载样式

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -20,6 +20,13 @@
 			<!-- node=widthFix:宽度不变,高度自动变化,保持原图宽高比不变 -->
 			<image src="../../static/images/nodata.png" mode="widthFix"></image>
 		</view>
+		
+		<!-- 当加载完新闻数据后，出现加载完的提示 -->
+		<view class="loading" v-if=newsArr.length>
+			<view></view>
+			<view v-if="loading == 1">数据加载中...</view>
+			<view v-if="loading == 2">数据加载完毕</view>
+		</view>
 	</view>
 </template>
 
@@ -28,9 +35,10 @@
 		data() {
 			return {
 				navIndex: 0,
-				navArr: [], //导航栏数据
-				newsArr: [] ,//新闻数据
+				navArr: [],     //导航栏数据
+				newsArr: [] ,   //新闻数据
 				currentPage:1,  //定义第一页新闻
+				loading:0,     //默认值为 0, 1 为加载中， 2 为加载完毕
 			}
 		},
 		// 第一次进入页面执行
@@ -38,10 +46,16 @@
 			this.getNavDat();
 			this.getNewsData();
 		},
+		// 页面触底时执行
 		onReachBottom(){
+			// 当loading 为 2 时,数据已加载完毕,不需要在发送请求
+			if(this.loading == 2){
+				return;
+			}
 			// 对触底后的 currentPage 进行加页,并且重新请求 currentPage为2 的新闻数据
 			// console.log("到底部了");
 			this.currentPage++;
+			this.loading = 1;
 			this.getNewsData();
 		},
 		methods: {
@@ -52,6 +66,8 @@
 				this.currentPage=1;
 				// 将新闻数据重新清空
 				this.newsArr=[];
+				// 切换页面后重置loading
+				this.loading = 0;
 				// console.log(id);  id为 50、51、52……
 				this.getNewsData(id);				
 			},
@@ -86,6 +102,9 @@
 					},
 					success: res => {
 						console.log(res);
+						if(res.data.length == 0){
+							this.loading = 2
+						}
 						// 将获取的新闻列表数据 放入 newsArr 数组中进行拼接
 						this.newsArr = [...this.newsArr,...res.data]
 					}
@@ -143,6 +162,15 @@
 		image {
 			width: 360rpx;
 
+		}
+	}
+	
+	.loading{
+		text-align: center;
+		font-size: 26rpx;
+		color: #888;
+		view{
+			padding: 20rpx 0;
 		}
 	}
 </style>
